f561b4746c98909327bbc80505cf7071996cc9dc
scap <scap@31066d9d-9628-c0ca-4048-6d22de68a070>
2008-12-02 22:00:53 +0000

    initial add.

--- Packet/packet.h
+++ Packet/packet.h
@@ -44,6 +44,21 @@ class Packet : public AppData
       appLayer_.push_back( a );
     }
 
+    template < class T > bool linkIs( int i )
+    {
+      return linkLayer_.at( i ).is< T >();
+    }
+
+    template < class T > bool inetIs( int i )
+    {
+      return inetLayer_.at( i ).is< T >();
+    }
+
+    template < class T > bool transIs( int i )
+    {
+      return transLayer_.at( i ).is< T >();
+    }
+
     int linkSize( ) const;
     int inetSize( ) const;
     int transSize( )const;
---
+++ Packet/packetBuilder.cpp
@@ -0,0 +1,95 @@
+#include "packetBuilder.h"
+//template specialization must go outside of class def
+template <> Packet PacketBuilder::build<Ethernet>( const uint8_t* buff, int size )
+{
+  Ethernet e( buff, size );
+  Packet p;
+  p.pushBackLink( e );
+  const uint8_t* newbuff = buff + e.getSize();
+  int newsize = size - e.getSize();
+  Packet p2;
+  switch( e.getType() )
+  {
+    case ethernetProtocol::ETH_P_IP:
+      //std::cerr << "IP" << std::endl;
+      p2 = build< IPv4 >( newbuff, newsize );
+      break;
+    case ethernetProtocol::ETH_P_IPV6:
+      //std::cerr << "IPv6" << std::endl;
+      //placeholder
+      //break;
+    case ethernetProtocol::ETH_P_ARP:
+      //std::cerr << "ARP" << std::endl;
+      //placeholder
+      //break;
+    default:
+      //std::cerr << "RAW" << std::endl;
+      p2 = build< Raw >( newbuff, newsize );
+      break;
+  }
+  return p + p2;
+}
+
+template <> Packet PacketBuilder::build<IPv4>( const uint8_t* buff, int size )
+{
+  IPv4 ip( buff, size );
+  Packet p; 
+  p.pushBackInet( ip );
+  Packet p2;
+  const uint8_t* newbuff = buff + p.getSize();
+  int newsize = size - p.getSize();
+  
+  switch( static_cast<uint16_t>( ip.getProtocol() ) )
+  {
+    case ipProtocols::IPPROTO_TCP:
+      p2 = build< TCP >( newbuff, newsize );
+      break;
+    case ipProtocols::IPPROTO_UDP:
+      p2 = build< TCP >( newbuff, newsize );
+      break;
+    default:
+      p2 = build< Raw >( newbuff, newsize );
+  }
+
+  return p + p2;
+}
+
+template <> Packet PacketBuilder::build<TCP>( const  uint8_t* buff, int size )
+{
+  TCP tcp( buff, size );
+  Packet p; 
+  p.pushBackTrans( tcp );
+  Packet p2;
+  const uint8_t* newbuff = buff + tcp.getSize();
+  int newsize = size - tcp.getSize();
+
+  //NO TCP APPLICATION LAYER SUPPORTED ATM
+  p2 = build< Raw >( newbuff, newsize );
+
+  return p + p2;
+}
+
+template <> Packet PacketBuilder::build<UDP>( const  uint8_t* buff, int size )
+{
+  UDP udp( buff, size );
+  Packet p; 
+  p.pushBackTrans( udp );
+  Packet p2;
+  const uint8_t* newbuff = buff + udp.getSize();
+  int newsize = size - udp.getSize();
+
+  //NO UDP APPLICATION LAYER SUPPORTED ATM
+  p2 = build< Raw >( newbuff, newsize );
+
+  return p + p2;
+}
+
+template <> Packet PacketBuilder::build<Raw>( const  uint8_t* buff, int size )
+{
+  Raw r( buff, size );
+  Packet p;
+  p.pushBackApp( r );
+
+  return p;
+}
+
