28e749d8d5a7bf36f4252578ef98f416dbd4a021
gaurav <gaurav@31066d9d-9628-c0ca-4048-6d22de68a070>
2008-12-03 18:37:02 +0000

    tcpDump functionality

---
+++ sniffer/tcpDump.cpp
@@ -0,0 +1,78 @@
+/**
+ * *
+ * * This program is free software: you can redistribute it and/or modify
+ * * it under the terms of the GNU General Public License as published by
+ * * the Free Software Foundation, either version 3 of the License, or
+ * * (at your option) any later version.
+ * *
+ * * This program is distributed in the hope that it will be useful,
+ * * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * * GNU General Public License for more details.
+ * *
+ * * You should have received a copy of the GNU General Public License
+ * * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include "sniffer.h"
+#include "snifferData.h"
+#include "filterData.h"
+#include "../common/commandLineParser.h"
+#include "../common/parseCommas.h"
+#include "../common/threads.h"
+#include "options.h"
+
+//std::string parser(int argc, char *argv[], std::string option);
+//void printHelp();
+
+
+int main(int argc, char *argv[])
+{
+	std::cout << "PACMAN v" << "0.0.0.0.1" << std::endl;
+	options tcpOptions;
+	tcpOptions.get_options(argc, argv);
+	tcpOptions.print_options();
+
+	std::vector<Thread *> threads;
+	int returnCode;
+	sniffer mySniffer;
+
+	//Check for debug
+	if(tcpOptions.is_debug)
+		mySniffer.setDebug(true);
+
+	mySniffer.log("tcpdump starting up!");
+
+	//check if we should print out pcap capabile devices
+	if ( tcpOptions.is_ls )
+	{
+		mySniffer.log( "Server started with -ls option printing devices and halting..." );
+		mySniffer.printDevices();
+		exit(-1);
+	}
+
+	//Creating different datastructures
+	mySniffer.log( "Creating Sniffer Thread(s)" );
+
+	if(tcpOptions.is_filter)
+		mySniffer.setFilter(std::string(tcpOptions.get_filter()));
+	if(tcpOptions.is_pcapDev)
+		mySniffer.setDevice(std::string(tcpOptions.get_pcapDev()));
+	else if(tcpOptions.is_inputPcapFile)
+		mySniffer.setInputPcapFile(std::string(tcpOptions.get_inputPcapFile()));
+	if(tcpOptions.is_outputPcapFile)
+		mySniffer.setOutPcapFile(std::string(tcpOptions.get_outputPcapFile()));
+	
+	Thread* thread = new Thread(run_sniffer);
+	thread->start( (void *) &mySniffer );
+	thread->join();
+	mySniffer.log( "tcpdump halting!" );
+	return 0;
+}
+
