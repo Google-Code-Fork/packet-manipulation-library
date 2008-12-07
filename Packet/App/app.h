00be3673a69a13dfad871d7f71784c14f5e9c866
scap <scap@31066d9d-9628-c0ca-4048-6d22de68a070>
2008-12-07 13:37:06 +0000

    More doxygen commenting

--- Packet/App/app.h
+++ Packet/App/app.h
@@ -1,3 +1,32 @@
+/*
+ * PacMan - Packet Manipulation Library 
+ * Copyright © 2008  Jeff Scaparra, Gaurav Yadav, Andrie Tanusetiawan
+ *
+ * This file is a part of PacMan.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** \file app.h
+ * This is the declaration for the class App
+ */
+
+/** \class App 
+ * This class is used in conjunction with Packet for handling App
+ *
+ */
+
 #ifndef APP_H 
 #define APP_H
 #include <stdexcept>
@@ -8,21 +37,25 @@
 class App : public Encapsulateable
 {
   public:
+    //!default constructor
     App( )
     {
       header_ = NULL;
     }
 
-    App( Raw p )
+    //!build an App from a Raw object
+    App( const Raw &p )
     {
       header_ = new Raw( p );
     }
 
+    //!copy constructor
     App( const App &n )
     {
       copy( n );
     }
 
+    //!equality operator
     App& operator=( const App &n )
     {
       if( header_ )
@@ -30,15 +63,21 @@ class App : public Encapsulateable
       copy( n );
       return *this;
     }
-    
+   
+    //!destructor
     virtual ~App() 
     {
       if( header_ )
       delete header_;
     }
 
+    //!is<some class> returns true if this is of that type 
+    //!example: is<TCP> returns true if TCP 
     template< class T >bool is( ){ return false; }
     
+    //!returns a T object
+    //!If this isn't a T object we throw runtime_error
+    //!example: is<TCP> returns true if TCP 
     template< class T > T get( ) 
     {
       if( !( is<T>() ) )
@@ -48,6 +87,7 @@ class App : public Encapsulateable
       return T(*((T*)header_));
     }
 
+    //!sets this App to a T
     template< class T > void set( T e )
     {
       if( header_ )
@@ -56,18 +96,22 @@ class App : public Encapsulateable
       *header_ = e;
     }
 
+    //!returns the size in bytes of the underlying data
     int getSize() const 
     {
       return header_->getSize();
     }
 
+    //!returns a packet buffer of the underlying datatype suitable for other
+    //libraries or code
     PacketBuffer makePacket() const 
     {
       return header_->makePacket();
     }
 
   private:
-   
+  
+    //!Used to copy different Application layers supported
     void copy( const App &n )
     {
       if( n.header_ == NULL )
@@ -82,6 +126,7 @@ class App : public Encapsulateable
 	header_ = NULL;
     }
 
+    //!internal data
     AppData* header_;
 };
 
--- Packet/App/raw.h
+++ Packet/App/raw.h
@@ -1,3 +1,33 @@
+/*
+ * PacMan - Packet Manipulation Library 
+ * Copyright © 2008  Jeff Scaparra, Gaurav Yadav, Andrie Tanusetiawan
+ *
+ * This file is a part of PacMan.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** \file raw.h
+ * This is the declaration for the class for handling Raw data
+ * This is a catch all class. This is also how the packet class can currently
+ * support unknown protocols. Unknown as in they are yet supported in PacMan. :)
+ */
+
+/** \class Raw
+ * This class is used in conjunction with Packet for handling anything.
+ *
+ */
 #ifndef RAW_H
 #define RAW_H
 
@@ -7,18 +37,30 @@
 class Raw : public AppData
 {
   public: 
+    //!default constructor
     Raw();
+    //!Build a Raw object from a byte buffer
     Raw( const uint8_t* data, int size );
+    //!copy constructor
     Raw( const Raw &n );
+    //!equality operator
     Raw& operator=( const Raw &n );
+    //!destructor
     virtual ~Raw();
+    //!Return a Packet Buffer holding the contents of the Raw object for use
+    //with other libraries or code
     PacketBuffer makePacket() const;
+    //!Return the size of the Raw contents in bytes.
     int getSize() const;
+    //!Set the raw data
     void setData( std::vector< uint8_t > data );
+    //!Get the raw data in a std::vector<uint8_t> format
     std::vector< uint8_t > getData() const;
+    //!Overloaded from App (returns true).
     bool isRaw() const {return true;}
 
   private:
+    //!internal data
     std::vector<uint8_t> data_;
 };
 
