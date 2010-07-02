02f7213a16867df4c7e86f321fc989562f7849d5
mkozma <mkozma@31066d9d-9628-c0ca-4048-6d22de68a070>
2010-07-02 18:47:08 +0000

    Working on creating seperate class to handle scr/dst IPv6 addresses [in progress...]

---
+++ Packet/Inet/ipv6Address.cpp
@@ -0,0 +1,132 @@
+#include <stdexcept>
+#include "ipv6Address.h"
+
+IPv6Address::IPv6Address() : address_( std::vector< uint8_t >(16,0) )
+{
+}
+
+IPv6Address::IPv6Address( uint8_t address[IPv6AddressSize] ) : address_(
+		std::vector< uint8_t >(16,0) )
+{
+	address_[0] = address[0];
+	address_[1] = address[1];
+	address_[2] = address[2];
+	address_[3] = address[3];
+	address_[4] = address[4];
+	address_[5] = address[5];
+	address_[6] = address[6];
+	address_[7] = address[7];
+	address_[8] = address[8];
+	address_[9] = address[9];
+	address_[10] = address[10];
+	address_[11] = address[11];
+	address_[12] = address[12];
+	address_[13] = address[13];
+	address_[14] = address[14];
+	address_[15] = address[15];
+}
+
+IPv6Address::IPv6Address( std::vector< uint8_t > address ) : address_(
+		std::vector< uint8_t >(16,0) )
+{
+	if( address.size() != 16 )
+		throw std::runtime_error( "IPv6 Address Size Incorrect" );
+	address_ = address;
+}
+
+IPv6Address::IPv6Address( const IPv6Address &a )
+{
+	address_ = a.address_;
+}
+
+IPv6Address& IPv6Address::operator=( const IPv6Address &a )
+{
+	address_ = a.address_;
+	return *this;
+}
+
+bool IPv6Address::operator==( const IPv6Address &a )
+{
+	if( address_[0] != a.address_[0] )
+		return false;
+	if( address_[1] != a.address_[1] )
+		return false;
+	if( address_[2] != a.address_[2] )
+		return false;
+	if( address_[3] != a.address_[3] )
+		return false;
+	if( address_[4] != a.address_[4] )
+		return false;
+	if( address_[5] != a.address_[5] )
+		return false;
+	if( address_[6] != a.address_[6] )
+		return false;
+	if( address_[7] != a.address_[7] )
+		return false;
+	if( address_[8] != a.address_[8] )
+		return false;
+	if( address_[9] != a.address_[9] )
+		return false;
+	if( address_[10] != a.address_[10] )
+		return false;
+	if( address_[11] != a.address_[11] )
+		return false;
+	if( address_[12] != a.address_[12] )
+		return false;
+	if( address_[13] != a.address_[13] )
+		return false;
+	if( address_[14] != a.address_[14] )
+		return false;
+	if( address_[15] != a.address_[15] )
+		return false;
+	return true;
+}
+
+IPv6Address::~IPv6Address()
+{
+}
+
+void IPv6Address::setIPv6Address( std::vector< uint8_t > address )
+{
+	if( address.size() != 16 )
+	{
+		throw std::runtime_error( "Invalid IPv6 address!" );
+	}
+	address_ = address;
+}
+
+int IPv6Address::size() const
+{
+	return IPv6AddressSize;
+}
+
+PacketBuffer IPv6Address::makePacket( ) const
+{
+	return PacketBuffer( address_ );
+}
+
+void IPv6Address::getIPv6( uint8_t (&addressPtr)[IPv6AddressSize] )
+{
+	addressPtr[0] = address_[0];
+	addressPtr[1] = address_[1];
+	addressPtr[2] = address_[2];
+	addressPtr[3] = address_[3];
+	addressPtr[4] = address_[4];
+	addressPtr[5] = address_[5];
+	addressPtr[6] = address_[6];
+	addressPtr[7] = address_[7];
+	addressPtr[8] = address_[8];
+	addressPtr[9] = address_[9];
+	addressPtr[10] = address_[10];
+	addressPtr[11] = address_[11];
+	addressPtr[12] = address_[12];
+	addressPtr[13] = address_[13];
+	addressPtr[14] = address_[14];
+	addressPtr[15] = address_[15];
+}
+
+bool IPv6Address::isIPv6() const
+{
+	return true;
+}
+
---
+++ Packet/Inet/ipv6Address.h
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include "ipv6Address.h"
+
+class IPv6Address
+{
+	public: //constants
+		//! Size of a IPv6 address in bytes
+		static const int IPv6AddressSize = 16;
+
+	public:
+		//! Default Constructor
+		IPv6Address();
+		//! Builds a IPv6Address with the bytes in IPv6
+		IPv6Address( uint8_t address[IPv6AddressSize] );
+		//! Builds a IPv6Address with the bytes in IPv6
+		IPv6Address( std::vector< uint8_t > address );
+		//! Copy Constructor
+		IPv6Address( const IPv6Address& a );
+		//! Assignment operator
+		IPv6Address& operator=( const IPv6Address &a );
+		//! Equality operation
+		bool operator == ( const IPv6Address &a );
+		//! Destructor
+		virtual ~IPv6Address();
+		
+		//! Sets the address base on the bytes in IPv6
+		void setAddress( std::vector< uint8_t > address );
+		//! Returns the size of the IPv6 address
+		int size() const;
+		//! Returns a Packetbuffer with the IPv6 address
+		PacketBuffer makePacket() const;
+		//! Places the IPv6 address in the array IPv6
+		void getIPv6( uint8_t (&address) [IPv6AddressSize]);
+		//! returns true
+		bool isIPv6() const;
+
+	private:
+		//! Private data of IPv6Address
+		std::vector< uint8_t > address_;
+};
+
