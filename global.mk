c650d14061bc119b990cd34721d6b256d4c1cea6
mkozma <mkozma@31066d9d-9628-c0ca-4048-6d22de68a070>
2010-09-03 13:39:20 +0000

    fixed the lib install directory.

--- global.mk
+++ global.mk
@@ -1,34 +1,34 @@
-PWD=/home/scap/PacMan
-PACKETDIR=/home/scap/PacMan/Packet
-INETDIR=/home/scap/PacMan/Packet/Inet
-LINKDIR=/home/scap/PacMan/Packet/Link
-TRANSDIR=/home/scap/PacMan/Packet/Trans
-APPDIR=/home/scap/PacMan/Packet/App
-APPSOURCE=/home/scap/PacMan/Packet/App/app.cpp /home/scap/PacMan/Packet/App/raw.cpp
-TRANSSOURCE=/home/scap/PacMan/Packet/Trans/tcp.cpp /home/scap/PacMan/Packet/Trans/udp.cpp /home/scap/PacMan/Packet/Trans/tcpOptions.cpp /home/scap/PacMan/Packet/Trans/trans.cpp
-INETSOURCE=/home/scap/PacMan/Packet/Inet/inet.cpp /home/scap/PacMan/Packet/Inet/ipv4.cpp /home/scap/PacMan/Packet/Inet/icmp.cpp
-LINKSOURCE=/home/scap/PacMan/Packet/Link/link.cpp /home/scap/PacMan/Packet/Link/ethernet.cpp /home/scap/PacMan/Packet/Link/mac.cpp
-PACKETSOURCE=/home/scap/PacMan/Packet/packetBuilder.cpp /home/scap/PacMan/Packet/packet.cpp /home/scap/PacMan/Packet/packetBuffer.cpp /home/scap/PacMan/Packet/App/app.cpp /home/scap/PacMan/Packet/App/raw.cpp /home/scap/PacMan/Packet/Trans/tcp.cpp /home/scap/PacMan/Packet/Trans/udp.cpp /home/scap/PacMan/Packet/Trans/tcpOptions.cpp /home/scap/PacMan/Packet/Trans/trans.cpp /home/scap/PacMan/Packet/Inet/inet.cpp /home/scap/PacMan/Packet/Inet/ipv4.cpp /home/scap/PacMan/Packet/Inet/icmp.cpp /home/scap/PacMan/Packet/Link/link.cpp /home/scap/PacMan/Packet/Link/ethernet.cpp /home/scap/PacMan/Packet/Link/mac.cpp
+PWD=/home/mkozma/tmp/PacMan
+PACKETDIR=/home/mkozma/tmp/PacMan/Packet
+INETDIR=/home/mkozma/tmp/PacMan/Packet/Inet
+LINKDIR=/home/mkozma/tmp/PacMan/Packet/Link
+TRANSDIR=/home/mkozma/tmp/PacMan/Packet/Trans
+APPDIR=/home/mkozma/tmp/PacMan/Packet/App
+APPSOURCE=/home/mkozma/tmp/PacMan/Packet/App/app.cpp /home/mkozma/tmp/PacMan/Packet/App/raw.cpp
+TRANSSOURCE=/home/mkozma/tmp/PacMan/Packet/Trans/tcp.cpp /home/mkozma/tmp/PacMan/Packet/Trans/udp.cpp /home/mkozma/tmp/PacMan/Packet/Trans/tcpOptions.cpp /home/mkozma/tmp/PacMan/Packet/Trans/trans.cpp
+INETSOURCE=/home/mkozma/tmp/PacMan/Packet/Inet/inet.cpp /home/mkozma/tmp/PacMan/Packet/Inet/ipv4.cpp /home/mkozma/tmp/PacMan/Packet/Inet/icmp.cpp
+LINKSOURCE=/home/mkozma/tmp/PacMan/Packet/Link/link.cpp /home/mkozma/tmp/PacMan/Packet/Link/ethernet.cpp /home/mkozma/tmp/PacMan/Packet/Link/mac.cpp
+PACKETSOURCE=/home/mkozma/tmp/PacMan/Packet/packetBuilder.cpp /home/mkozma/tmp/PacMan/Packet/packet.cpp /home/mkozma/tmp/PacMan/Packet/packetBuffer.cpp /home/mkozma/tmp/PacMan/Packet/App/app.cpp /home/mkozma/tmp/PacMan/Packet/App/raw.cpp /home/mkozma/tmp/PacMan/Packet/Trans/tcp.cpp /home/mkozma/tmp/PacMan/Packet/Trans/udp.cpp /home/mkozma/tmp/PacMan/Packet/Trans/tcpOptions.cpp /home/mkozma/tmp/PacMan/Packet/Trans/trans.cpp /home/mkozma/tmp/PacMan/Packet/Inet/inet.cpp /home/mkozma/tmp/PacMan/Packet/Inet/ipv4.cpp /home/mkozma/tmp/PacMan/Packet/Inet/icmp.cpp /home/mkozma/tmp/PacMan/Packet/Link/link.cpp /home/mkozma/tmp/PacMan/Packet/Link/ethernet.cpp /home/mkozma/tmp/PacMan/Packet/Link/mac.cpp
 APPOBJS=$(APPSOURCE:%.cpp=%.o)
 TRANSOBJS=$(TRANSSOURCE:%.cpp=%.o)
 INETOBJS=$(INETSOURCE:%.cpp=%.o)
 LINKOBJS=$(LINKSOURCE:%.cpp=%.o)
 PACKETOBJS=$(PACKETSOURCE:%.cpp=%.o)
-APPHEADERS=/home/scap/PacMan/Packet/App/appData.h /home/scap/PacMan/Packet/App/app.h /home/scap/PacMan/Packet/App/raw.h
-TRANSHEADERS=/home/scap/PacMan/Packet/Trans/tcp.h /home/scap/PacMan/Packet/Trans/udp.h /home/scap/PacMan/Packet/Trans/tcpOptions.h /home/scap/PacMan/Packet/Trans/trans.h /home/scap/PacMan/Packet/Trans/transData.h /home/scap/PacMan/Packet/Trans/eolOption.h /home/scap/PacMan/Packet/Trans/mssOption.h /home/scap/PacMan/Packet/Trans/noopOption.h /home/scap/PacMan/Packet/Trans/sackOption.h /home/scap/PacMan/Packet/Trans/sackPermittedOption.h /home/scap/PacMan/Packet/Trans/timeStampOption.h /home/scap/PacMan/Packet/Trans/wsOption.h
-INETHEADERS=/home/scap/PacMan/Packet/Inet/inetData.h /home/scap/PacMan/Packet/Inet/inet.h /home/scap/PacMan/Packet/Inet/ipv4.h /home/scap/PacMan/Packet/Inet/icmp.h
-LINKHEADERS=/home/scap/PacMan/Packet/Link/linkData.h /home/scap/PacMan/Packet/Link/link.h /home/scap/PacMan/Packet/Link/ethernet.h /home/scap/PacMan/Packet/Link/mac.h
-PACKETHEADERS=/home/scap/PacMan/Packet/packetBuilder.h /home/scap/PacMan/Packet/packet.h /home/scap/PacMan/Packet/packetBuffer.h /home/scap/PacMan/Packet/App/appData.h /home/scap/PacMan/Packet/App/app.h /home/scap/PacMan/Packet/App/raw.h /home/scap/PacMan/Packet/Trans/tcp.h /home/scap/PacMan/Packet/Trans/udp.h /home/scap/PacMan/Packet/Trans/tcpOptions.h /home/scap/PacMan/Packet/Trans/trans.h /home/scap/PacMan/Packet/Trans/transData.h /home/scap/PacMan/Packet/Trans/eolOption.h /home/scap/PacMan/Packet/Trans/mssOption.h /home/scap/PacMan/Packet/Trans/noopOption.h /home/scap/PacMan/Packet/Trans/sackOption.h /home/scap/PacMan/Packet/Trans/sackPermittedOption.h /home/scap/PacMan/Packet/Trans/timeStampOption.h /home/scap/PacMan/Packet/Trans/wsOption.h /home/scap/PacMan/Packet/Inet/inetData.h /home/scap/PacMan/Packet/Inet/inet.h /home/scap/PacMan/Packet/Inet/ipv4.h /home/scap/PacMan/Packet/Inet/icmp.h /home/scap/PacMan/Packet/Link/linkData.h /home/scap/PacMan/Packet/Link/link.h /home/scap/PacMan/Packet/Link/ethernet.h /home/scap/PacMan/Packet/Link/mac.h
-PACKETTESTDIR=/home/scap/PacMan/Packet/TestCode
-INETTESTDIR=/home/scap/PacMan/Packet/Inet/TestCode
-LINKTESTDIR=/home/scap/PacMan/Packet/Link/TestCode
-TRANSTESTDIR=/home/scap/PacMan/Packet/Trans/TestCode
-APPTESTDIR=/home/scap/PacMan/Packet/App/TestCode
+APPHEADERS=/home/mkozma/tmp/PacMan/Packet/App/appData.h /home/mkozma/tmp/PacMan/Packet/App/app.h /home/mkozma/tmp/PacMan/Packet/App/raw.h
+TRANSHEADERS=/home/mkozma/tmp/PacMan/Packet/Trans/tcp.h /home/mkozma/tmp/PacMan/Packet/Trans/udp.h /home/mkozma/tmp/PacMan/Packet/Trans/tcpOptions.h /home/mkozma/tmp/PacMan/Packet/Trans/trans.h /home/mkozma/tmp/PacMan/Packet/Trans/transData.h /home/mkozma/tmp/PacMan/Packet/Trans/eolOption.h /home/mkozma/tmp/PacMan/Packet/Trans/mssOption.h /home/mkozma/tmp/PacMan/Packet/Trans/noopOption.h /home/mkozma/tmp/PacMan/Packet/Trans/sackOption.h /home/mkozma/tmp/PacMan/Packet/Trans/sackPermittedOption.h /home/mkozma/tmp/PacMan/Packet/Trans/timeStampOption.h /home/mkozma/tmp/PacMan/Packet/Trans/wsOption.h
+INETHEADERS=/home/mkozma/tmp/PacMan/Packet/Inet/inetData.h /home/mkozma/tmp/PacMan/Packet/Inet/inet.h /home/mkozma/tmp/PacMan/Packet/Inet/ipv4.h /home/mkozma/tmp/PacMan/Packet/Inet/icmp.h
+LINKHEADERS=/home/mkozma/tmp/PacMan/Packet/Link/linkData.h /home/mkozma/tmp/PacMan/Packet/Link/link.h /home/mkozma/tmp/PacMan/Packet/Link/ethernet.h /home/mkozma/tmp/PacMan/Packet/Link/mac.h
+PACKETHEADERS=/home/mkozma/tmp/PacMan/Packet/packetBuilder.h /home/mkozma/tmp/PacMan/Packet/packet.h /home/mkozma/tmp/PacMan/Packet/packetBuffer.h /home/mkozma/tmp/PacMan/Packet/App/appData.h /home/mkozma/tmp/PacMan/Packet/App/app.h /home/mkozma/tmp/PacMan/Packet/App/raw.h /home/mkozma/tmp/PacMan/Packet/Trans/tcp.h /home/mkozma/tmp/PacMan/Packet/Trans/udp.h /home/mkozma/tmp/PacMan/Packet/Trans/tcpOptions.h /home/mkozma/tmp/PacMan/Packet/Trans/trans.h /home/mkozma/tmp/PacMan/Packet/Trans/transData.h /home/mkozma/tmp/PacMan/Packet/Trans/eolOption.h /home/mkozma/tmp/PacMan/Packet/Trans/mssOption.h /home/mkozma/tmp/PacMan/Packet/Trans/noopOption.h /home/mkozma/tmp/PacMan/Packet/Trans/sackOption.h /home/mkozma/tmp/PacMan/Packet/Trans/sackPermittedOption.h /home/mkozma/tmp/PacMan/Packet/Trans/timeStampOption.h /home/mkozma/tmp/PacMan/Packet/Trans/wsOption.h /home/mkozma/tmp/PacMan/Packet/Inet/inetData.h /home/mkozma/tmp/PacMan/Packet/Inet/inet.h /home/mkozma/tmp/PacMan/Packet/Inet/ipv4.h /home/mkozma/tmp/PacMan/Packet/Inet/icmp.h /home/mkozma/tmp/PacMan/Packet/Link/linkData.h /home/mkozma/tmp/PacMan/Packet/Link/link.h /home/mkozma/tmp/PacMan/Packet/Link/ethernet.h /home/mkozma/tmp/PacMan/Packet/Link/mac.h
+PACKETTESTDIR=/home/mkozma/tmp/PacMan/Packet/TestCode
+INETTESTDIR=/home/mkozma/tmp/PacMan/Packet/Inet/TestCode
+LINKTESTDIR=/home/mkozma/tmp/PacMan/Packet/Link/TestCode
+TRANSTESTDIR=/home/mkozma/tmp/PacMan/Packet/Trans/TestCode
+APPTESTDIR=/home/mkozma/tmp/PacMan/Packet/App/TestCode
 PACKETTESTSOURCE=
-APPTESTSOURCE=/home/scap/PacMan/Packet/App/TestCode/testRaw.cpp
-TRANSTESTSOURCE=/home/scap/PacMan/Packet/Trans/TestCode/testTcp.cpp /home/scap/PacMan/Packet/Trans/TestCode/testUdp.cpp
-INETTESTSOURCE=/home/scap/PacMan/Packet/Inet/TestCode/testIcmp.cpp /home/scap/PacMan/Packet/Inet/TestCode/testIPv4.cpp
-LINKTESTSOURCE=/home/scap/PacMan/Packet/Link/TestCode/ethernetTest.cpp /home/scap/PacMan/Packet/Link/TestCode/macTest.cpp
+APPTESTSOURCE=/home/mkozma/tmp/PacMan/Packet/App/TestCode/testRaw.cpp
+TRANSTESTSOURCE=/home/mkozma/tmp/PacMan/Packet/Trans/TestCode/testTcp.cpp /home/mkozma/tmp/PacMan/Packet/Trans/TestCode/testUdp.cpp
+INETTESTSOURCE=/home/mkozma/tmp/PacMan/Packet/Inet/TestCode/testIcmp.cpp /home/mkozma/tmp/PacMan/Packet/Inet/TestCode/testIPv4.cpp
+LINKTESTSOURCE=/home/mkozma/tmp/PacMan/Packet/Link/TestCode/ethernetTest.cpp /home/mkozma/tmp/PacMan/Packet/Link/TestCode/macTest.cpp
 PACKETTESTOBJS=$(PACKETTESTSOURCE:%.cpp=%.o)
 APPTESTOBJS=$(APPTESTSOURCE:%.cpp=%.o)
 TRANSTESTOBJS=$(TRANSTESTSOURCE:%.cpp=%.o)
