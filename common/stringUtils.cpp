a20988a6938fc0cac4d1ba60fa5d602d77973bbc
jeff.scaparra <jeff.scaparra@31066d9d-9628-c0ca-4048-6d22de68a070>
2011-05-09 19:34:44 +0000

    Initial add

---
+++ common/stringUtils.cpp
@@ -0,0 +1,42 @@
+/**
+ * * Copyright © 2011 Jeff Scaparra
+ * *
+ * * This file is a part of the PacMan project.
+ * *
+ * * This program is free software: you can redistribute it and/or modify
+ * * it under the terms of the GNU General Public License as published by
+ * * the Free Software Foundation, either version 3 of the License, or
+ * * (at your option) any later version.
+ * *
+ * * This program is distributed in the hope that it will be useful,
+ * * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * * GNU General Public License for more details.
+ * *
+ * * You should have received a copy of the GNU General Public License
+ * * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+
+#include "stringUtils.h"
+
+std::vector< std::string > split( const std::string &string, const char &del )
+{
+	std::vector< std::string > stringList;
+	std::string tmpString;
+	for( int i = 0; i < string.size(); ++i )
+	{
+		if( string[i] != del )
+		{
+			tmpString += string[i];
+		}
+		else
+		{
+			stringList.push_back( tmpString );
+			tmpString = "";
+		}
+	}
+	if( tmpString.size() )
+		stringList.push_back( tmpString );
+
+	return stringList;
+}
