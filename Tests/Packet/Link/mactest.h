00620fc70d573aa30d845df13c0cc870b2d68f0e
Jeff.Scaparra <Jeff.Scaparra@31066d9d-9628-c0ca-4048-6d22de68a070>
2011-05-20 19:20:28 +0000

    fixing paths now that files are moved around.

--- Tests/Packet/Link/mactest.h
+++ Tests/Packet/Link/mactest.h
@@ -4,10 +4,10 @@
 #include <QObject>
 #include <QtCore/QString>
 #include <QtTest/QtTest>
-#include "../../../../Packet/Link/ethernet.h"
-#include "../../../../Packet/Link/linkData.h"
-#include "../../../../Packet/Link/mac.h"
-#include "../../../../Packet/Link/link.h"
+#include "../../../Packet/Link/ethernet.h"
+#include "../../../Packet/Link/linkData.h"
+#include "../../../Packet/Link/mac.h"
+#include "../../../Packet/Link/link.h"
 class MacTest : public QObject
 {
     Q_OBJECT
--- Tests/Packet/Link/tst_ethernettest.h
+++ Tests/Packet/Link/tst_ethernettest.h
@@ -2,9 +2,9 @@
 #define TST_ETHERNETTEST_H
 #include <QtCore/QString>
 #include <QtTest/QtTest>
-#include "../../../../Packet/Link/ethernet.h"
-#include "../../../../Packet/Link/linkData.h"
-#include "../../../../Packet/Link/mac.h"
+#include "../../../Packet/Link/ethernet.h"
+#include "../../../Packet/Link/linkData.h"
+#include "../../../Packet/Link/mac.h"
 
 class EthernetTest : public QObject
 {
