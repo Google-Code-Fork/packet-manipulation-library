7f58f4ed9f47e9bade4ccf4826c1bddd0c777d4a
jeff.scaparra <jeff.scaparra@31066d9d-9628-c0ca-4048-6d22de68a070>
2011-07-19 23:55:46 +0000

    Arping is working. Fixed the IPv4Address class so that the constructor uses htonl on uint32_t ips. :) Comming together well.

--- Packet/Inet/ipv4address.cpp
+++ Packet/Inet/ipv4address.cpp
@@ -29,7 +29,7 @@ IPv4Address::IPv4Address( std::vector< uint8_t > address )
   }
 }
 
-IPv4Address::IPv4Address( const uint32_t &ip ): ip_( ip )
+IPv4Address::IPv4Address( const uint32_t &ip ): ip_( htonl(ip) )
 {
 }
 
